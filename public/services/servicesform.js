10784140a18d1f66f6911c31c6c42c1f6ef7a421
hilla19.fh <hillaryfwd19@gmail.com>
2025-08-27 12:01:10 -0600

    html

---
+++ public/services/servicesform.js
@@ -0,0 +1,82 @@
+async function getcondiciones(endpoint) {
+      try {
+            const response = await fetch(`http://localhost:3001/${endpoint}`, {
+                  method: 'GET',
+                  headers: {
+                        'Content-Type': 'application/json'
+                  }
+
+            })
+
+            const condiciones = await response.json()
+
+            return condiciones
+
+      } catch (error) {
+
+            console.error("Hay un error al obtener las condiciones", error)
+            throw error
+
+      }
+
+}
+export { getcondiciones }
+    
+async function postcondiciones(objcondiciones) {
+      try {
+            const response = await fetch('', {
+                  method: 'POST',
+                  headers: {
+                        'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(objcondiciones)
+            });
+            const condiciones = await response.json()
+            return condiciones
+      } catch (error) {
+            console.error("Hay un error al obtener las condiciones", error)
+            throw error
+
+      }
+}
+export { postcondiciones }
+
+
+async function putcondiciones(objcondiciones) {
+      try {
+            const response = await fetch('', {
+                  method: 'PATCH',
+                  headers: {
+                        'Content-Type': 'application/json'
+                  },
+                  
+            });
+            const condiciones = await response.json()
+            return condiciones
+      } catch (error) {
+            console.error("Hay un error al obtener las condiciones", error)
+            throw error
+
+      }
+}
+export { putcondiciones }
+
+
+async function deletecondiciones(objcondiciones) {
+      try {
+            const response = await fetch('', {
+                  method: 'DELETE',
+                  headers: {
+                        'Content-Type': 'application/json'
+                  },
+                  
+            });
+            const condiciones = await response.json()
+            return condiciones
+      } catch (error) {
+            console.error("Hay un error al obtener las condiciones", error)
+            throw error
+
+      }
+}
+export { deletecondiciones }
