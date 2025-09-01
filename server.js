9479325efeacb9b560fdd3d621161dec5cf300cb
hilla19.fh <hillaryfwd19@gmail.com>
2025-09-01 08:11:36 -0600

    commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'pages/index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'pages/registro.html'));
 });
 
 const PORT = 3000;
